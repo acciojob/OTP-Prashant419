33f20ec4adac390e77fdb7832332ea2a2b36f7a9
Prashant Sharma <97109392+Prashant419@users.noreply.github.com>
2024-02-04 18:09:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+function moveToNextInput(currentInput) {
+  if (currentInput.value && currentInput.nextElementSibling) {
+    currentInput.nextElementSibling.focus();
+  } else if (!currentInput.value && currentInput.previousElementSibling) {
+    currentInput.previousElementSibling.focus();
+  }
+}
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Backspace') {
+    const focusedInput = document.activeElement;
+    if (!focusedInput.value && focusedInput.previousElementSibling) {
+      focusedInput.previousElementSibling.focus();
+    }
+  }
+});//your JS code here. If required.
+function moveToNextInput(currentInput) {
+  if (currentInput.value && currentInput.nextElementSibling) {
+    currentInput.nextElementSibling.focus();
+  } else if (!currentInput.value && currentInput.previousElementSibling) {
+    currentInput.previousElementSibling.focus();
+  }
+}
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Backspace') {
+    const focusedInput = document.activeElement;
+    if (!focusedInput.value && focusedInput.previousElementSibling) {
+      focusedInput.previousElementSibling.focus();
+    }
+  }
+});
